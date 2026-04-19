d75d5af7c005b45704ebd54411df619875d3880a
Silaje123 <Michelstivesilajengouontche@gmail.com>
2026-04-19 16:21:56 -0400

    Implémentation de la couche Data (DAO) et tests dans main.py

--- main.py
+++ main.py
@@ -3,24 +3,24 @@ from Models.salle import Salle
 
 dao = DataSalle()
 
-# Test connexion
+
 print("Connexion OK")
 
-# Ajouter une salle
+
 s = Salle("S001", "Labo Info", "Laboratoire", 30)
 dao.insert_salle(s)
 
-# Récupérer toutes les salles
+
 for salle in dao.get_salles():
     salle.afficher_infos()
 
-# Rechercher une salle
+
 s2 = dao.get_salle("S001")
 print(s2.description)
 
-# Modifier
+
 s2.capacite = 40
 dao.update_salle(s2)
 
-# Supprimer
+
 dao.delete_salle("S001")
